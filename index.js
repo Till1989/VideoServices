196bb8baa16482ac21f5aa9fa61884406a40e69a
Till1989 <131892010+Till1989@users.noreply.github.com>
2023-07-07 22:39:54 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+import {VideoService} from './classes/VideoService.js';
+import {Channel} from './classes/Channel.js';
+import {FullVideo, Shorts} from './classes/Video.js';
+import { User } from './classes/User.js';
+import { vidPage, vidServPage, channPage } from './classes/Page.js';
+
+let vidSrv;
+
+
+
+
+
+
+
+
+vidSrv = new VideoService("YouTube");
+vidSrv.AddChannel(new Channel("ВелоМотоОгород"));
+vidSrv.channels[vidSrv.channels.findIndex(ch=>ch.name=="ВелоМотоОгород")].AddVideo(new FullVideo("Посадил и офигел! 100% работает! Не спам!"));
+vidSrv.channels[vidSrv.channels.findIndex(ch=>ch.name=="ВелоМотоОгород")].AddVideo(new Shorts("Приехал на работу и, бл#, домой хочу"));
+console.log(vidSrv);
+
+
+let vSPage=new vidServPage(new User("sfffda",false),vidSrv);
+//vSPage.user=new User("aaa",true);
+console.log(vSPage);
+
+let chPage=new channPage(vSPage.user,vSPage.channels[vidSrv.channels.findIndex(ch=>ch.name=="ВелоМотоОгород")]);
+chPage.Subscribe();
+
+
+
+let vPage=new vidPage(chPage.user,chPage.videos[chPage.videos.findIndex(vid=>vid.name=="Посадил и офигел! 100% работает! Не спам!")]);
+vPage.Like();
+vPage.Dislike();
+vPage.Comment("Вам положена выплата 10000грн. Перейдите по ссылке и получите!");
+vPage.Comment("Наебалово, как обычно!");
+vPage.Comment("Только что получила, даже больше! Спасибо большое!");
+vPage.Uncomment("Наебалово, как обычно!");
+
+chPage.Unsubscribe();
+
+console.log(vPage);
+
+vPage=new vidPage(chPage.user,chPage.videos[chPage.videos.findIndex(vid=>vid.name=="Приехал на работу и, бл#, домой хочу")]);
+vPage.Like();
+vPage.Comment("Та же фигня(((");
+
+chPage.Unsubscribe();
+
+console.log(vPage);
+
+
+
+
+
+
+
+
